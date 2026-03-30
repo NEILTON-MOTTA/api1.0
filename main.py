cceb6aa6141a297e25d4c15ff1874fbe42a488f8
Neilton <neilton@dynacomp.com.br>
2026-03-30 16:34:03 -0300

    fix routes package

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from routes import valida_usuario       # importa valida_usuario.py
 from routes import numerador_clientes   # importa valida_usuario.py
 
 app = FastAPI()
+@app.get("/")
+def root():
+    return {"status": "ok"}
 
 app.include_router(cliente.router)
 app.include_router(valida_usuario.router)
