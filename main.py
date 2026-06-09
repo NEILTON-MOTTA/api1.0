2afd864db3d4f968b852e7cd32ccfb46b80a239f
Neilton <neilton@dynacomp.com.br>
2026-06-09 16:52:33 -0300

    Atualizacao 03062026

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from routes import cliente              # importa cliente.py
 from routes import valida_usuario       # importa valida_usuario.py
 from routes import numerador_clientes   # importa valida_usuario.py
 from routes import produto              # importa produto.py
-
+from routes import controle_app         # importa controle_app.py
 #app = FastAPI()
 app = FastAPI(
-    title="API  X6"
+    title="API  X7"
 )
 
 @app.get("/teste-versao 2026.06.03-002")
@@ -18,4 +18,6 @@ app.include_router(cliente.router)
 app.include_router(valida_usuario.router)
 app.include_router(numerador_clientes.router)
 app.include_router(produto.router)
+app.include_router(controle_app.router)
+
 
