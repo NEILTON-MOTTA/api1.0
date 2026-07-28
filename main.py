f270117c9631f522d4ec8268c2e3e602a3693f80
Neilton <neilton@dynacomp.com.br>
2026-07-27 21:54:46 -0300

    Atualizacao 03062026

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from routes import valida_usuario       # importa valida_usuario.py
 from routes import numerador_clientes   # importa valida_usuario.py
 from routes import produto              # importa produto.py
 from routes import adm                  # importa adm.py
+from routes import controle_app         # importa controle_app.py
 
 app = FastAPI()
 
@@ -12,4 +13,5 @@ app.include_router(valida_usuario.router)
 app.include_router(numerador_clientes.router)
 app.include_router(produto.router)
 app.include_router(adm.router)
+app.include_router(controle_app.router)
 
